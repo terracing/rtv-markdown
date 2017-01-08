3f94bb23b0ef94aa1a054dc887b40525d432b3af
terracing <terrazas.omar@gmail.com>
2017-01-08 13:40:50 -0600

    Integración con ShareJS.

--- server.js
+++ server.js
@@ -9,5 +9,18 @@ app.get('/', function(req, res){
     res.render('pad');
 });
 
+app.get('/(:id)', function(req, res){
+    res.render('pad');
+});
+
+var sharejs = require('share');
+require('redis');
+
+var options = {
+    db: {type: 'redis'},
+};
+
+sharejs.server.attach(app, options);
+
 var port = process.env.PORT || 8000;
 app.listen(port);
