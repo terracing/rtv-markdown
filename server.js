6d672472f460fbe01e1eda365a34a435665852ea
terracing <terrazas.omar@gmail.com>
2017-01-08 10:43:10 -0600

    Configuración del servidor.

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('pad');
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port);
